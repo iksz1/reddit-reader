6692ba5c5c1e84210ba2184857ed883a0d11c748
iksz1 <accuratno@gmail.com>
2018-09-11 15:05:03 +0300

    refactor Home

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Redirect, RouteComponentProps } from "@reach/router";
+import { IAppState } from "../store/ducks";
 
-const Home: React.SFC<RouteComponentProps> = () => {
-  return <Redirect to="/r/reactjs" noThrow={true} />;
-};
+type PFS = ReturnType<typeof mapStateToProps>;
 
-export default Home;
+interface IProps extends PFS, RouteComponentProps {}
+
+const Home = ({ subreddit }: IProps) => <Redirect to={`/r/${subreddit}`} noThrow />;
+
+const mapStateToProps = ({ subs }: IAppState) => ({
+  subreddit: subs[0],
+});
+
+export default connect(mapStateToProps)(Home);
